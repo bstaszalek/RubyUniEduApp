fdbeaa797bdce6233c62979f3c211930b64c0e84
BStaszalek <bartosz.staszalek@gmail.com>
2015-04-28 13:04:38 +0000

    add some restriction to updating pictures for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,7 @@ class Article < ActiveRecord::Base
                     length: { minimum: 10 }
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb =>"100x100>"}
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  
+  validates_attachment_file_name :picture, :matches => [/gif\Z/, /png\Z/, /jpe?g\Z/]
+  validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 1.megabytes
 end
